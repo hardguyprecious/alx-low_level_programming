373047e38aa555354a4aeadae8a3d4a076937720
Precious Ejims <ejimsprecious4@gmail.com>
2023-02-16 13:18:19 -0800

    Hello C done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - This is Entry point
  *
  * Return: always 0
  */
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - will be the main function
+ * main - Entry point
  *
  * Return: 0 if success.
  */
