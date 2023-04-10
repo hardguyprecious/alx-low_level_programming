ac67a7b5d8468e68f74030855fe96fafc1069ad3
Precious Ejims <ejimsprecious4@gmail.com>
2023-04-10 11:08:39 -0700

    task 0 fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,36 +3,35 @@
 /**
  * binary_to_uint - converts a binary number to an
  * unsigned int.
- * @b: binary string.
+ * @b: binary.
  *
- * Return: converted number, or 0 if b is NULL or
- * contains an invalid character.
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	int len = 0;
-	int base = 1;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while (b[len] != '\0')
-		len++;
+	ui = 0;
 
-	len--;
+	for (len = 0; b[len] != '\0'; len++)
+		;
 
-	while (len >= 0)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
+		}
 
-		if (b[len] == '1')
-			result += base;
-
-		base *= 2;
-		len--;
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
 
-	return (result);
+	return (ui);
 }
